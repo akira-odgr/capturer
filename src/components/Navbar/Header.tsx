11c54cf656cd9ad70009b40a26edd2c37965f0f4
akira-odgr <177723350+akira-odgr@users.noreply.github.com>
2025-01-19 23:00:54 +0900

    Headerセクションが完成しました。

--- src/components/Navbar/Header.tsx
+++ src/components/Navbar/Header.tsx
@@ -1,46 +1,59 @@
-import HeaderLogoWhite from "../../assets/images/logo-white.png";
-import HeaderLogoDark from "../../assets/images/logo-dark.png";
+import LogoWhite from "../../assets/images/logo-white.png";
+// import LogoDark from "../../assets/images/logo-dark.png";
 
 import "./Header.scss";
+import { useState } from "react";
 
 export const Header = () => {
+    const [isActive, setIsActive] = useState<boolean>(false);
+
+    const toggle = () => {
+        setIsActive(!isActive);
+    };
     return (
         <header>
-            <nav>
+            <nav className="nav">
                 <div className="nav__header">
-                    <img
-                        src={HeaderLogoWhite}
-                        alt="header-logo"
-                        className="nav__header__logo"
-                    />
-                    <img
-                        src={HeaderLogoDark}
-                        alt="header-logo"
-                        className="nav__header__logo"
-                    />
-                </div>
-                <div className="nav__menu__btn">
-                    <span></span>
-                    <span></span>
-                    <span></span>
+                    <div className="nav__header__logo">
+                        <img src={LogoWhite} alt="header-logo" />
+                    </div>
+                    <div
+                        className={`nav__header__btn ${
+                            isActive ? "is-active" : ""
+                        }`}
+                        onClick={toggle}
+                    >
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                    </div>
                 </div>
 
-                <div className="nav__links">
-                    <ul>
-                        <li>
-                            <a href=""></a>
-                        </li>
-                        <li>
-                            <a href=""></a>
-                        </li>
-                        <li>
-                            <a href=""></a>
-                        </li>
-                        <li>
-                            <a href=""></a>
-                        </li>
-                    </ul>
-                </div>
+                <ul className={`nav__links ${isActive ? " is-active" : ""}`}>
+                    <li>
+                        <a href="#home">home</a>
+                    </li>
+                    <li>
+                        <a href="#about">about us</a>
+                    </li>
+                    <li>
+                        <a href="#services">services</a>
+                    </li>
+                    <li className="nav__links__logo">
+                        <a href="#">
+                            <img src={LogoWhite} alt="logo-white" />
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#client">client</a>
+                    </li>
+                    <li>
+                        <a href="#blog">blog</a>
+                    </li>
+                    <li>
+                        <a href="#contact">contact</a>
+                    </li>
+                </ul>
             </nav>
         </header>
     );
