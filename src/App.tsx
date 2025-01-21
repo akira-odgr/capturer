1cde7550b01276525c17978844881da446939322
akira-odgr <177723350+akira-odgr@users.noreply.github.com>
2025-01-21 20:37:15 +0900

    App.tsx内のportfolioのパスを修正しました。

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { About } from "./components/about/About";
 import { Header } from "./components/header/Header";
-import { Portfolio } from "./components/portfolio/Portfolio";
 import { Service } from "./components/service/Service";
+import { Portfolio } from "./components/portfolio/Portfolio";
 import { Client } from "./components/client/Client";
 
 import "./App.scss";
