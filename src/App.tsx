13374766ec405f48eecefb9e9f82982762db9c78
akira-odgr <177723350+akira-odgr@users.noreply.github.com>
2025-01-21 21:20:12 +0900

    Appファイルを編集。

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { About } from "./components/about/About";
 import { Header } from "./components/header/Header";
 import { Service } from "./components/service/Service";
-import { Portfolio } from "./components/portfolio/Portfolio";
+// import { Portfolio } from "./components/portfolio/Portfolio";
 import { Client } from "./components/client/Client";
 
 import "./App.scss";
@@ -11,7 +11,7 @@ export const App = () => {
         <>
             <Header />
             <About />
-            <Portfolio />
+            {/* <Portfolio /> */}
             <Service />
             <Client />
         </>
