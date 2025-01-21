a764c98874a967c3658f97fca20c3cbdfc0adc47
akira-odgr <177723350+akira-odgr@users.noreply.github.com>
2025-01-21 21:44:33 +0900

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
 import { About } from "./components/about/About";
 import { Header } from "./components/header/Header";
 import { Service } from "./components/service/Service";
-// import { Portfolio } from "./components/portfolio/Portfolio";
 import { Client } from "./components/client/Client";
 
 import "./App.scss";
+import { Portfolio } from "./components/portfolio/Portfolio";
 
 export const App = () => {
     return (
         <>
             <Header />
             <About />
-            {/* <Portfolio /> */}
             <Service />
+            <Portfolio />
             <Client />
         </>
     );
