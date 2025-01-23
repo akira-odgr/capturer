730bffc56973ba7532c722721e2f06590bcbc391
akira-odgr <177723350+akira-odgr@users.noreply.github.com>
2025-01-23 10:33:27 +0900

    Loading画面を追加しました。

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import { useEffect, useState } from "react";
+
 import { About } from "./components/about/About";
 import { Header } from "./components/header/Header";
 import { Service } from "./components/service/Service";
@@ -10,16 +12,40 @@ import { Instagram } from "./components/instagram/Instagram";
 import "./App.scss";
 
 export const App = () => {
+    const [loading, setLoading] = useState(true); // ローディング状態を管理
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setLoading(false); // 3秒後にローディングを終了
+        }, 3000);
+        return () => clearTimeout(timer); // クリーンアップ
+
+        //     // データ取得や処理をシミュレート
+        //     const fetchData = async () => {
+        //         // ここでデータを取得する処理を行う
+        //         // 例: await fetch("api_endpoint");
+        //         setLoading(false); // ロードが完了したらローディングを終了
+        //     };
+
+        //     fetchData(); // データ取得を実行
+    }, []);
+
     return (
         <>
-            <Header />
-            <About />
-            <Service />
-            <Portfolio />
-            <Client />
-            <Gallery />
-            <Blog />
-            <Instagram />
+            {loading ? ( // ローディング中はLoadingメッセージを表示
+                <div className="loader"></div>
+            ) : (
+                <div className="fade-in">
+                    <Header />
+                    <About />
+                    <Service />
+                    <Portfolio />
+                    <Client />
+                    <Gallery />
+                    <Blog />
+                    <Instagram />
+                </div>
+            )}
         </>
     );
 };
